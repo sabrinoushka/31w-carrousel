b1cb0e799773f51f3439c101865c31660df939cf
Olena <e2195610@cmaisonneuve.qc.ca>
2022-07-05 15:07:11 -0400

    Modification du fichier 'carrousel.js'

--- script/carrousel.js
+++ script/carrousel.js
@@ -1,16 +1,16 @@
 (function(){
-
-    console.log("debut du carrousel");
-    //let elmGalerie = document.querySelector(".galerie")
-    //console.log(elmGalerie);
-
-    let elmGalerie__img = document.querySelectorAll('.galerie figure img')
-    console.log(elmGalerie__img);
-
-    for (const img of elmGalerie__img) {
+    console.log('Début du carrousel')
+   
+    let elmGalerie = document.querySelector('.galerie')
+    let elmGalerieImg = elmGalerie.querySelectorAll("figure figure img")
+    let elmCarrousel = document.querySelector('.carrousel')
+    let elmCarrousel__figure = document.querySelector('.carrousel__figure')
+    console.log(elmGalerieImg[0].getAttribute('src'))
+    for (const img of elmGalerieImg){
         img.addEventListener('mousedown', function(){
-            console.log(img.getAttribute('src'))
-        })       
+            console.log(this.getAttribute('src'))
+            elmCarrousel.classList.add('carrousel__active')
+            console.log(elmCarrousel.classList)
+        })
     }
-
 })()
